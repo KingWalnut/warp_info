5a4e382fc6cc8f3969553b8d56f8a12befe93a83
KingWalnut <50270826+KingWalnut@users.noreply.github.com>
2020-12-13 15:49:29 -0800

    Add files via upload

--- warp_info/items.sql
+++ warp_info/items.sql
@@ -1,2 +1,3 @@
-INSERT INTO `vorp`.`items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('wood', 'Wood', 50, 1, 'item_standard', 1);
-INSERT INTO `vorp`.`items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('wooden_boards', 'Wooden Boards', 25, 1, 'item_standard', 1);
+INSERT INTO `vorp`.`items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('book1', 'Reciepe Book 1', 50, 1, 'item_standard', 1);
+INSERT INTO `vorp`.`items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('book2', 'Reciepe Book 2', 25, 1, 'item_standard', 1);
+INSERT INTO `vorp`.`items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('book3', 'Reciepe Book 3', 50, 1, 'item_standard', 1);
