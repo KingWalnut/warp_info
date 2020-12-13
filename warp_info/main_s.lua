e019b91e7db4b1bd8bb4bf59c624b0394d7f24ec
KingWalnut <50270826+KingWalnut@users.noreply.github.com>
2020-12-13 15:49:40 -0800

    Add files via upload

--- warp_info/main_s.lua
+++ warp_info/main_s.lua
@@ -7,25 +7,31 @@ end)
 VorpInv = exports.vorp_inventory:vorp_inventoryApi()
 
 
-VorpInv.RegisterUsableItem("someshit", function(data)
+VorpInv.RegisterUsableItem("book1", function(data)
 
-    VorpInv.subItem(data.source, "someshit", 1)
+    VorpInv.subItem(data.source, "book1", 1)
     local Character = VorpCore.getUser(data.source).getUsedCharacter
     local sender1 = Character.firstname 
     local sender2 = Character.lastname
     TriggerClientEvent('warp_info:Vulture', data.source, sender1, sender2)
 end)
 
-VorpInv.RegisterUsableItem("someshit2", function(data)
+VorpInv.RegisterUsableItem("book2", function(data)
 
-    VorpInv.subItem(data.source, "someshit2", 1)
-    TriggerClientEvent('warp_info:Vulture2', data.source)
+    VorpInv.subItem(data.source, "book2", 1)
+    local Character = VorpCore.getUser(data.source).getUsedCharacter
+    local sender1 = Character.firstname 
+    local sender2 = Character.lastname
+    TriggerClientEvent('warp_info:Vulture2', data.source, sender1, sender2)
 end)
 
 
-VorpInv.RegisterUsableItem("someshit3", function(data)
+VorpInv.RegisterUsableItem("book3", function(data)
 
-    VorpInv.subItem(data.source, "someshit3", 1)
-    TriggerClientEvent('warp_info:Vulture3', data.source)
+    VorpInv.subItem(data.source, "book3", 1)
+    local Character = VorpCore.getUser(data.source).getUsedCharacter
+    local sender1 = Character.firstname 
+    local sender2 = Character.lastname
+    TriggerClientEvent('warp_info:Vulture3', data.source, sender1, sender2)
 end)
 
