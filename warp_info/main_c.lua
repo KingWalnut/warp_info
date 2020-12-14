c8d6a40179e7e5b17b8b9ad9543a556c99b6488e
KingWalnut <50270826+KingWalnut@users.noreply.github.com>
2020-12-13 16:04:43 -0800

    Add files via upload

--- warp_info/main_c.lua
+++ warp_info/main_c.lua
@@ -1,11 +1,18 @@
+telegram = false
+
 
 RegisterNetEvent('warp_info:Vulture')
 AddEventHandler('warp_info:Vulture', function(firstname, lastname)
   local randomitempull = math.random(1, #Config.reciepeItems)
   local itemName = Config.reciepeItems[randomitempull]
-  TriggerEvent("vorp:TipRight", "Reciepe: ".. itemName, 20000)
-  local message = itemName
-  TriggerServerEvent("Telegram:SendMessage", firstname, lastname, message, GetPlayerServerIds())
+  if telegram then
+	  TriggerEvent("vorp:TipRight", "Reciepe: ".. itemName, 20000)
+	  local message = itemName
+	  TriggerServerEvent("Telegram:SendMessage", firstname, lastname, message, GetPlayerServerIds())
+	else
+		TriggerEvent("vorp:TipRight", "Reciepe: ".. itemName, 20000)
+	end
+	  
 end)
 
 
@@ -15,14 +22,26 @@ RegisterNetEvent('warp_info:Vulture2')
 AddEventHandler('warp_info:Vulture2', function()
   local randomitempull = math.random(1, #Config.reciepeItems2)
   local itemName = Config.reciepeItems2[randomitempull]
-  TriggerEvent("vorp:TipRight", "Reciepe: ".. itemName, 20000)   
+  if telegram then
+	  TriggerEvent("vorp:TipRight", "Reciepe: ".. itemName, 20000)
+	  local message = itemName
+	  TriggerServerEvent("Telegram:SendMessage", firstname, lastname, message, GetPlayerServerIds())
+	else
+		TriggerEvent("vorp:TipRight", "Reciepe: ".. itemName, 20000)
+	end
 end)
 
 RegisterNetEvent('warp_info:Vulture3')
 AddEventHandler('warp_info:Vulture3', function()
   local randomitempull = math.random(1, #Config.reciepeItems3)
   local itemName = Config.reciepeItems3[randomitempull]
-  TriggerEvent("vorp:TipRight", "Reciepe: ".. itemName, 20000)   
+  if telegram then
+	  TriggerEvent("vorp:TipRight", "Reciepe: ".. itemName, 20000)
+	  local message = itemName
+	  TriggerServerEvent("Telegram:SendMessage", firstname, lastname, message, GetPlayerServerIds())
+	else
+		TriggerEvent("vorp:TipRight", "Reciepe: ".. itemName, 20000)
+	end  
 end)
 
 
